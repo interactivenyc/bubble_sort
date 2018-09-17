81e65fdbb4a7dbdad9bafd1b84eb37f16d2ef5d6
Steve Warren <steve@interactivenyc.com>
2018-09-17 13:51:46 -0400

    createSpy working

--- bubble.js
+++ bubble.js
@@ -4,15 +4,20 @@ function bubbleSort(array) {
 
     for (let i = 0; i < len; i++) {        
         if (sortHappened === false || len === 0) {   
+            console.log('FOUND BEST ARRAY', array);
+            
             return array;
         }
+        
         sortHappened = doSort(array)
+        console.log('----------------Trip ', i, sortHappened);
+        console.log('array in progress: ', array);
     }
     return array;
 }
 
 function doSort(array) {
-    let sortHappened;
+    let sortHappened = false;
 
     for (let j = 0; j < array.length - 1; j++) {
         if (array[j] > array[j+1]) {
@@ -20,8 +25,11 @@ function doSort(array) {
             array[j] = array[j+1];
             array[j+1] = tmp;
 
+            // console.log('Did a sort!');
+            
             sortHappened = true;
         }
     }
+    
     return sortHappened;
 }
--- bubble.spec.js
+++ bubble.spec.js
@@ -1,19 +1,27 @@
 describe('Bubble Sort', function(){
 
   beforeAll(function () {
+    // window.doSort.calls.reset();
     spyOn(window, 'doSort').and.callThrough();
-  });
-
-  
-    it('handles an empty array', function(){
-      expect( bubbleSort([]) ).toEqual( [] );
-    });
-
-    it('sorts this array', function() {
-        expect( bubbleSort([6, 2, 3, 1, 5, 4]) ).toEqual([1, 2, 3, 4, 5, 6])
-    })
-
-    console.log('doSort was called: ', window.doSort.calls.count());
     
-
   });
+  
+  it('handles an empty array', function(){
+    expect( bubbleSort([]) ).toEqual( [] );
+    console.log('doSort was called: ', window.doSort.calls.count());
+    window.doSort.calls.reset();
+  });
+
+  it('sorts this array', function() {
+      expect( bubbleSort([6, 2, 3, 1, 5, 4]) ).toEqual([1, 2, 3, 4, 5, 6])
+      console.log('doSort was called: ', window.doSort.calls.count());
+      window.doSort.calls.reset();
+  })
+
+  it('sorts this array', function() {
+    expect( bubbleSort([8, 9, 6, 2, 3, 7, 1, 5, 4]) ).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    console.log('doSort was called: ', window.doSort.calls.count());
+    window.doSort.calls.reset();
+  })
+
+});
